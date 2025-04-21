05174d53d4744673d093f8f1f26c1ef42a007b2e
Vítor Oliveira Almeida <144623215+v-almeida@users.noreply.github.com>
2025-04-21 17:21:02 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -5,7 +5,7 @@ public class App {
         int b = 10;
         int soma = a + b;
         String nome = "Vitor";
-        String ola = "Olá, tudo bem?" +  nome + "!" ;
+        String ola = "Olá, Mundo" +  nome + "!" ;
 
         
         System.out.println(ola);
